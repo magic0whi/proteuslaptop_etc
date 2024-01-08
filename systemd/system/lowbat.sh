52b38cb948244e4774beb8d856f8711adcb26926
proteus <lollipop.studio.cn@gmail.com>
2024-01-08 19:25:17 +0800

    feat(lowbat): Replace 'cat' with builtin '<' redirect
    Signed-off-by: proteus <lollipop.studio.cn@gmail.com>

--- systemd/system/lowbat.sh
+++ systemd/system/lowbat.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-status=$(cat /sys/class/power_supply/BAT0/status)
-capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+status=$(< /sys/class/power_supply/BAT0/status)
+capacity=$(< /sys/class/power_supply/BAT0/capacity)
 
 if [ "$status" = "Discharging" -a "$capacity" -lt 10 ]; then
     logger "Critical battery threshold"
